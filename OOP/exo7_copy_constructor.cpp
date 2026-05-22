cb2e92d24f33c6547efe8eaa3a6f01624ff2ba1f
PegasusWare13 <anwesh.082bct013@tcioe.edu.np>
2026-05-22 04:31:07 +0000

    copy constructor program

--- OOP/exo7_copy_constructor.cpp
+++ OOP/exo7_copy_constructor.cpp
@@ -8,14 +8,21 @@ class Point
   private:
     int x, y;
   public:
-    void display()
+    Point(int x1, int y1):x(x1), y(y1){};//constructor
+    int getter_x()
     {
-      cout<<"The value of x = "<<x<<"The value of y = "<<y;
+      return x;
+    }
+    int getter_y()
+    {
+      return y;
     }
 };
 int main()
 {
-  Point P1;
-  
+  Point P1(3,1);
+  cout<<"For P1: x = "<<P1.getter_x()<<" y = "<<P1.getter_y()<<endl;
+  Point P2(P1); //Or I can do Point P2 = P1. No difference between what I used and the assignment operator in this case
+  cout<<"For P2: x = "<<P2.getter_x()<<" y = "<<P2.getter_y()<<endl;
   return 0;
 }
